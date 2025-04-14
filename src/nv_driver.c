5b3109a76e077cdd8418b733739652d54aaba9ca
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-14 11:27:17 +0200

    replace XNFcalloc() by calloc()
    Standard C calloc() really is enough here, no need to extra Xserver
    wrappers that are in process of being phased out anyways.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/nv_driver.c
+++ src/nv_driver.c
@@ -695,7 +695,10 @@ NVGetRec(ScrnInfoPtr pScrn)
     if (pScrn->driverPrivate != NULL)
         return TRUE;
 
-    pScrn->driverPrivate = XNFcalloc(sizeof(NVRec));
+    pScrn->driverPrivate = calloc(1, sizeof(NVRec));
+    if (!(pScrn->driverPrivate))
+        return FALSE;
+
     /* Initialise it */
 
     return TRUE;
@@ -1929,7 +1932,9 @@ NVPreInit(ScrnInfoPtr pScrn, int flags)
      * and what sort of modes they can be used for.
      */
 
-    clockRanges = XNFcalloc(sizeof(ClockRange));
+    clockRanges = calloc(1, sizeof(ClockRange));
+    if (!clockRanges)
+        return FALSE;
     clockRanges->next = NULL;
     clockRanges->minClock = pNv->MinVClockFreqKHz;
     clockRanges->maxClock = pNv->MaxVClockFreqKHz;
