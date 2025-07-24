X11Libre/xf86-video-nv

commit b1b6f64bde0215c13e22615553a79f433368af80

g80: dont set accelmethod to xaa when xaa is disabled
Signed-off-by: Yusuf Khan <yusisamerican@gmail.com>
Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-nv/-/merge_requests/15>

src/g80_driver.c
@@ -281,9 +281,12 @@ G80PreInit(ScrnInfoPtr pScrn, int flags)
         xf86DrvMsg(pScrn->scrnIndex, X_CONFIG, "Acceleration disabled\n");
     }
     s = xf86GetOptValString(pNv->Options, OPTION_ACCEL_METHOD);
+#ifdef HAVE_XAA_H
     if(!s || !strcasecmp(s, "xaa"))
         pNv->AccelMethod = XAA;
-    else if(!strcasecmp(s, "exa"))
+    else
+#endif
+    if(!strcasecmp(s, "exa"))
         pNv->AccelMethod = EXA;
     else {
         xf86DrvMsg(pScrn->scrnIndex, X_ERROR, "Unrecognized AccelMethod "

commit 1e72f5804433382683790441d84312bfcb8c0cfe

g80: Avoid segfault if AccelMethod isn't set and XAA isn't built
Fixes: b1b6f64 ("g80: dont set accelmethod to xaa when xaa is disabled")
Reviewed-by: Yusuf Khan <yusisamerican@gmail.com>
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-nv/-/merge_requests/27>

src/g80_driver.c
@@ -284,9 +284,10 @@ G80PreInit(ScrnInfoPtr pScrn, int flags)
 #ifdef HAVE_XAA_H
     if(!s || !strcasecmp(s, "xaa"))
         pNv->AccelMethod = XAA;
-    else
+    else if(!strcasecmp(s, "exa"))
+#else
+    if(!s || !strcasecmp(s, "exa"))
 #endif
-    if(!strcasecmp(s, "exa"))
         pNv->AccelMethod = EXA;
     else {
         xf86DrvMsg(pScrn->scrnIndex, X_ERROR, "Unrecognized AccelMethod "